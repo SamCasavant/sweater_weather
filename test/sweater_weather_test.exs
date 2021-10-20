ea793ec3b3eccb7dcc481a35289ae4d1846c2e1c
Samuel Casavant <samuel.j.casavant@gmail.com>
2021-10-20 18:21:11 -0400

    Test for SweaterWeather.get_state_code

--- test/sweater_weather_test.exs
+++ test/sweater_weather_test.exs
@@ -2,7 +2,9 @@ defmodule SweaterWeatherTest do
   use ExUnit.Case
   doctest SweaterWeather
 
-  test "greets the world" do
-    assert SweaterWeather.hello() == :world
+  test "gets state code" do
+    assert SweaterWeather.get_state_code("ohio") == "US-OH"
+    assert SweaterWeather.get_state_code("DISTRICT OF COLUMBIA") == "US-DC"
+    assert SweaterWeather.get_state_code("Northern Mariana Islands") == "US-MP"
   end
 end
