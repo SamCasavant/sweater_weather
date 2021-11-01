76e999f2ca22a0899ceb4c26180199b6533f2a8f
Samuel Casavant <samuel.j.casavant@gmail.com>
2021-11-01 14:46:31 -0400

    Corrected some typos, added '&units=imperial' to api request (updated sample data accordingly)

--- lib/sweater_weather.ex
+++ lib/sweater_weather.ex
@@ -7,7 +7,7 @@ defmodule SweaterWeather do
     ## Parameters
       - city: full name of user's city
       - state: ISO 3166-2 State Code
-      - api-key: API key for OpenWeatherMaps.org
+      - api-key: API key for OpenWeatherMap.org
 
     ## Examples
       iex> SweaterWeather.get_advice("columbus", "US-OH", :test)
@@ -28,43 +28,49 @@ defmodule SweaterWeather do
     city_url = String.split(city, " ") |> Enum.join("%20")
 
     query_url =
-      'http://api.openweathermap.org/data/2.5/forecast?q=#{city_url},#{state_code}&appid=#{api_key}'
+      'http://api.openweathermap.org/data/2.5/forecast?q=#{city_url},#{state_code}&units=imperial&appid=#{api_key}'
 
     case api_key do
       :test ->
         IO.puts(query_url)
-        {:ok, JSON.decode(File.read!('sample_data/sample_weather_query.json'))}
+        JSON.decode(File.read!('sample_data/sample_weather_query.json'))
 
       _ ->
         {:ok, {{_, 200, 'OK'}, _headers, weather_json}} =
           :httpc.request(:get, {query_url, []}, [], body_format: :string)
 
-        {:ok, JSON.decode(weather_json)}
+        JSON.decode(weather_json)
     end
   end
 
   @doc """
-  Takes weather JSON data and returns high, low, and weather conditions across a time span.
+  Takes weather decoded JSON data and returns high, low, and weather conditions.
 
-  Parameters:
-    weather_json: Decoded json response from OpenWeatherMaps.org
-    date: 0 = today, 1 = tomorrow .. 4 = four days from now
-    start_time, end_time: The endpoints of a range in local, military time hours to use for weather measurement.
-                          start_time is inclusive, end_time is exclusive
 
-  Issues:
-    This will not account for times outside of the data on a given date, ie. if the date provided is today and the start_time has already passed,
-    the resulting issues are considered out of scope for this prototype and aren't handled.
   Examples:
 
-  iex> output = File.read!('sample_data/sample_weather_query.json') |> JSON.decode() |> parse_weather(1, 9, 17)
+  iex> output = File.read!('sample_data/sample_weather_query.json') |> JSON.decode() |> restrict_weather_range() |> parse_weather()
     {:ok, high, low, [conditions]}
   """
-  def parse_weather(weather_json, date \\ 1, start_time \\ 9, end_time \\ 17) do
+  def parse_weather(weather_map) do
   end
-end
 
-defmodule CLI do
+  @doc """
+  Takes a decoded json response from OpenWeatherMap.org, returns data trimmed to specified time range.
+
+  Parameters:
+    date: 0 = today, 1 = tomorrow .. 4 = four days from now
+    start_time, end_time: The endpoints of a range in local, military time hours. start_time is inclusive, end_time is exclusive.
+  Issues:
+    This will not account for times outside of the data on a given date, ie. if the date provided is today and the start_time has already passed,
+    the resulting issues are considered out of scope for this prototype and aren't handled."""
+  def restrict_weather_range(weather_map, date \\ 1, start_time \\ 9, end_time \\ 17) do
+    current_date_utc = DateTime.utc_now
+    current_date_local = DateTime.shift_zone!(current_date_utc, local_time_zone)
+  end
+  end
+
+  defmodule CLI do
   @doc """
   escript executable function for interfacing with this module.
 
@@ -72,7 +78,7 @@ defmodule CLI do
     Arguments can be passed in with flags or during program execution.
     - city: full name of user's city
     - state: user's state formatted as "ohio", "OH", or "US-OH"
-    - api-key: API key for OpenWeatherMaps.org
+    - api-key: API key for OpenWeatherMap.org
 
   ## Examples
 
